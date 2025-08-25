df0d35190fc0755326bbe8ed2a1636f45297ac5b
Jonatan <jonatan.shaya99@gmail.com>
2025-08-25 13:41:07 +0300

    Initial FocusFlow links app: header + 3 buttons + deploy config

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>FocusFlow</h1>
+      </header>
+      <main className="App-main">
+        <a href="https://the-jar-team.github.io/focus-flow-client/#/?isGuest=true" className="App-button">
+          Enter Site
+        </a>
+        <a href="https://the-jar-team.github.io/focus-flow-admin/#/dashboard" className="App-button">
+          Site Register/Login
+        </a>
+        <a href="https://the-jar-team.github.io/Engagement-Monitoring/" className="App-button">
+          Engagment Model Demo View
+        </a>
+      </main>
+    </div>
+  );
+}
+
+export default App;
